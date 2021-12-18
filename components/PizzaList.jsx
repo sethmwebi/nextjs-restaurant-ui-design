2790b779eaeb1574909e69bdf04af0eb317246d3
Seth Mwebi <sethmwebi27@gmail.com>
2021-12-18 12:46:24 +0300

    first commit

---
+++ components/PizzaList.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styles from "../styles/PizzaList.module.css"
+import PizzaCard from "./PizzaCard"
+
+const PizzaList = () => {
+	return (
+		<div className={styles.container}>
+			<h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
+			<p className={styles.desc}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore, quam quaerat eaque sed aut praesentium veritatis placeat quae, nam omnis mollitia nemo, porro sit fugit ratione provident iure sapiente tenetur.</p>
+			<div className={styles.wrapper}>
+				<PizzaCard />
+				<PizzaCard />
+				<PizzaCard />
+				<PizzaCard />
+				<PizzaCard />
+				<PizzaCard />
+				<PizzaCard />
+				<PizzaCard />
+			</div>
+		</div>
+	)
+}
+
+export default PizzaList
